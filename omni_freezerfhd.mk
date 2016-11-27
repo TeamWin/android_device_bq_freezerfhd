4da9c3c6b49dffbae8f91eb2774d56113349fd57
Kra1o5 <kra1o5x@gmail.com>
2016-11-27 18:54:27 +0100

    Update to M
    Change-Id: If2a4ae1224f323273200754eba85b3fda197f240

--- omni_freezerfhd.mk
+++ omni_freezerfhd.mk
@@ -1,9 +1,20 @@
-# Inherit device configuration
-$(call inherit-product, device/bq/freezerfhd/device.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := device/bq/freezerfhd/kernel
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata \
+    device/bq/freezerfhd/kernel:kernel
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := freezerfhd
 PRODUCT_NAME := omni_freezerfhd
 PRODUCT_BRAND := bq
-PRODUCT_MANUFACTURER := bq
 PRODUCT_MODEL := Aquaris M10 FHD
+PRODUCT_MANUFACTURER := bq
+
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=Aquaris_M10_FHD PRODUCT_NAME=Aquaris_M10_FHD
